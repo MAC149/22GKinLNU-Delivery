9156bead78e96c8d50db86e2e01790eac5e4d97f
MAC149 <95559610+MAC149@users.noreply.github.com>
2022-07-21 04:51:26 +0800

    7.21Moding

---
+++ Test/Servo.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+
+void Servo1_Init(int dat)
+{
+    servo1.attach(dat);
+}
+
+void Servo2_Init(int dat)
+{
+    servo2.attach(dat);
+}
+
+void Servo1_Writes(int pos,int delays)//pos 0-180
+{
+    servo1.write(pos);
+    delay(delays);
+}
+
+void Servo2_Writes(int pos,int delays)//pos 0-180
+{
+    servo2.write(pos);
+    delay(delays);
+}
+
+void Servo1_Writems(int pos,int delayms)//pos 0-180
+{
+    servo1.write(pos);
+    delayMicroseconds(delayms);
+}
+
+void Servo2_Writems(int pos,int delays)//pos 0-180
+{
+    servo2.write(pos);
+    delayMicroseconds(delayms);
+}
